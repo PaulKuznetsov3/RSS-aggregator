a75db7c82bf53ea03271320b0f5d9366d5c5d9e1
Павел Кузнецов <pavel_iv37@mail.ru>
2023-03-11 16:14:14 +0300

    step2.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import init from './init.js';
+import app from './init.js';
 import './style.scss';
 
-init();
+app();
--- src/init.js
+++ src/init.js
@@ -1,9 +1,82 @@
 // @ts-check
 
-import Example from './Example.js';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import keyBy from 'lodash/keyBy.js';
+import isEmpty from 'lodash/isEmpty.js';
+import renderError from './view.js';
 
-export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
+const validate = (url, urlsList) => {
+  try {
+    const schema = yup.string()
+      .url('Ссылка должна быть валидным URL')
+      .required()
+      .notOneOf(urlsList, 'RSS уже существует');
+
+    return schema.validate(url, { abortEarly: false });
+  } catch (e) {
+    console.log(e)
+    return keyBy(e.inner, 'path');
+  }
+ 
+  };
+
+ 
+const app = () => {
+
+    const state = {
+        form: {
+            process: 'fill',
+            valid: false,
+            errors: {},
+            url: '',
+            feeds: []
+        }
+    }
+
+    const elements = {
+        form: document.querySelector('.rss-form'),
+        input: document.querySelector('#url-input'),
+        button: document.querySelector('button'),
+        feedback: document.querySelector('.feedback')
+      }
+
+    const watchedState = onChange(state, (path, value, previousValue) => {
+      console.log(path)
+        switch(path) {
+          case 'form.errors': {
+            renderError(elements, watchedState.form.errors)
+            break;
+          }
+        }
+    })
+
+    elements.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const url = formData.get('url');
+     
+  
+      validate(url, watchedState.form.feeds).then((url) => {
+        watchedState.form.errors = 'RSS успешно загружен';
+        watchedState.form.valid = true;
+        watchedState.form.feeds = [...watchedState.form.feeds, url]
+        console.log(watchedState.form.feeds)
+        
+        
+        elements.form.reset()
+      })
+      .catch((err) => {
+      
+        const errors  = err.message
+        watchedState.form.errors = errors;
+        watchedState.form.valid = false;
+       
+      })
+     
+    })
+  
+
 };
+
+export default app;
